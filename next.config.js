0e5be40a8b085eb8f3107959ef1dcdc4a02c2fcc
harsh virani <harshvirani.91@gmail.com>
2022-11-24 19:33:16 +0530

    ..

--- next.config.js
+++ next.config.js
@@ -5,3 +5,11 @@ dotenvLoad();
 
 const withNextEnv = nextEnv();
 module.exports = withNextEnv();
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
